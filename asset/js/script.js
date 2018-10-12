f8cca65da25ed9be9fb0a099c1aa88113046539d
karichalobato <00002517@uca.edu.sv>
2018-10-12 02:31:02 -0600

    labo 7

---
+++ asset/js/script.js
@@ -0,0 +1,11 @@
+var container = document.getElementById("c1");
+container.innerText = "¡Hola Mundo!";
+container.style.background = "#000000";
+container.style.color = "#1cb723";
+container.style.width = "200px";
+container.style.height = "200px";
+container.innerHTML = "<input type='text' placeholder = 'escriba su texto'>"
+
+var containers = document.getElementsByClassName("c2");
+containers.
+containers.innerHTML = "<input type='text' placeholder='Escriba su texto'>"
