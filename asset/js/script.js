96f36144afdd9962414fcfc2ee10e8cc310139db
karichalobato <37193483+karichalobato@users.noreply.github.com>
2018-10-12 09:08:40 -0600

    Update script.js

--- asset/js/script.js
+++ asset/js/script.js
@@ -6,6 +6,26 @@ container.style.width = "200px";
 container.style.height = "200px";
 container.innerHTML = "<input type='text' placeholder = 'escriba su texto'>"
 
+
+
 var containers = document.getElementsByClassName("c2");
-containers.
-containers.innerHTML = "<input type='text' placeholder='Escriba su texto'>"
+containers.forEach(function(element) {
+ element.style.backgroundColor="#009999";    
+});
+containers.forEach(function(element) {
+    element.innerHTML= "<input type='text' placeholder='escriba su texto'></input>";
+});
+
+
+var boton= document.getElementById("btn-click");
+   boton.onclick=function(evt){
+    alert("Hola mundo"); 
+   };
+
+var  btnCopy=document.getElementById("btnCopy");
+btnCopy.onclick = function(evt)
+{
+  let msj = document.getElementById('textMsj').value;
+  document.getElementById('showMsj').innerText = msj;
+  } 
+  
